10c70a3fe05ccfcdd5a3b6fff6891effaabf0c38
Alexis <reche06@gmail.com>
2019-10-02 22:29:55 -0400

    finished project and done w/o stretches cause im being lazy

--- assignments/objects.js
+++ assignments/objects.js
@@ -3,19 +3,47 @@
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
-// 1, mmelloy0@psu.edu, Mitzi, F
-// 2, kdiben1@tinypic.com, Kennan, M
-// 3, kmummery2@wikimedia.org, Keven, M
-// 4, gmartinson3@illinois.edu, Gannie, M
-// 5, adaine5@samsung.com, Antonietta, F
+// 1,mmelloy0@psu.edu,Mitzi,F
+// 2,kdiben1@tinypic.com,Kennan,M
+// 3,kmummery2@wikimedia.org,Keven,M
+// 4,gmartinson3@illinois.edu,Gannie,M
+// 5,adaine5@samsung.com,Antonietta,F
 
-// Example format of an intern object: 1, examples@you.edu, Example, F
-const example = {
-  id: 0,
-  name: "Example",
-  email: "examples@you.edu",
-  gender: "F",
+// Example format of an intern object: 1,examples@you.edu,Example,F
+const inter1= {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
 }
+const inter2= {
+  "id": 2,
+  "name": "kenna",
+  "email":"kdiben1@tinypic.com",
+  "gender": "M"
+}
+const inter3= {
+  "id": 3,
+  "name": "Kevan",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+const inter4= {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}
+const inter5= {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F"
+}
+
+
+
+
 
 // Write your intern objects here:
 
@@ -24,23 +52,31 @@ const example = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(inter1.name);
 
 // Kennan's ID
-
+console.log(inter2.id);
 // Keven's email
-
+console.log(inter4.email);
 // Gannie's name
-
+console.log(inter4.name);
 // Antonietta's Gender
-
+console.log(inter5.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+console.log("Hello my name is " + inter3.name +"!");
+//or
+inter2.speak = function toSpeak() {
+  return "Hello, my name is "+ inter2.name;
+}
+console.log(inter2.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+inter5.multiplyNums = (num1, num2) => (num1 * num2);
 
-// === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
+console.log(inter5.multiplyNums(3,4));
+// === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
 
@@ -49,16 +85,3 @@ const example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
-
-// Log the parent object's name
-
-// Log the child's age
-
-// Log the name and age of the grandchild
-
-// Have the parent speak
-
-// Have the child speak
-
-// Have the grandchild speak
